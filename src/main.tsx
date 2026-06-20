a045ad2c0e729ae3b6ec196969ef63cf18f2ce4d
snehakapilavayi <snehakapilavayi07@gmail.com>
2026-06-20 11:40:00 +0530

    feat: Clear browser localStorage on first load to remove all old history

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// One-time programmatic clear to wipe all old logged browser data/history
+if (!localStorage.getItem("paisa.cleared.v2")) {
+  localStorage.clear();
+  localStorage.setItem("paisa.cleared.v2", "true");
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
